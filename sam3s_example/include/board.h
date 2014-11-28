a42c11f8767fcb01548992b91ef2d3c3bc49390d
Christina Quast <chrysh.ng+git@gmail.com>
2014-11-28 16:38:26 +0100

    simtrace board specific defines

--- sam3s_example/include/board.h
+++ sam3s_example/include/board.h
@@ -5,9 +5,9 @@
 #include "syscalls.h" /** RedHat Newlib minimal stub */
 
 /** Name of the board */
-#define BOARD_NAME "SAM3S-EK"
+#define BOARD_NAME "SAM3S-SIMTRACE"
 /** Board definition */
-#define sam3sek
+#define simtrace
 /** Family definition (already defined) */
 #define sam3s
 /** Core definition */
@@ -17,7 +17,18 @@
 #define BOARD_MAINOSC 12000000
 #define BOARD_MCK     48000000
 
-//#define LED_STATUS PIO_PA8
-#define LED_STATUS PIO_PA17
+#define LED_RED PIO_PA17
+#define LED_GREEN PIO_PA18
+
+/** USART0 pin RX */
+#define PIN_USART0_RXD    {PIO_PA9A_URXD0, PIOA, ID_PIOA, PIO_PERIPH_A, PIO_DEFAULT}
+/** USART0 pin TX */
+#define PIN_USART0_TXD    {PIO_PA10A_UTXD0, PIOA, ID_PIOA, PIO_PERIPH_A, PIO_DEFAULT}
+
+#define BOARD_PIN_USART_RXD     PIN_USART0_RXD
+#define BOARD_PIN_USART_TXD     PIN_USART0_TXD
+
+#define BOARD_ID_USART          ID_USART0
+#define BOARD_USART_BASE        USART0
 
 #endif

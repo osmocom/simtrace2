1edf350c34489599713996e0fdf9bbb965c1b0b3
Christina Quast <chrysh.ng+git@gmail.com>
2015-02-27 13:33:52 +0100

    MITM code and run and init decl in header

--- sam3s_example/simtrace/simtrace.h
+++ sam3s_example/simtrace/simtrace.h
@@ -8,12 +8,19 @@ enum confNum {
 // FIXME: static function definitions
 extern uint32_t _ISO7816_GetChar( uint8_t *pCharToReceive );
 extern uint32_t _ISO7816_SendChar( uint8_t CharToSend );
-extern void Phone_Master_Init( void );
 
+/*  Init functions   */
+extern void Phone_Master_Init( void );
 extern void Sniffer_Init( void );
+extern void MITM_init( void );
 
 extern void SIMtrace_USB_Initialize( void );
-
 extern void _ISO7816_Init( void );
 
-#endif
+/*  Run functions   */
+extern void Sniffer_run( void );
+// extern void CCID_run( void );
+extern void Phone_run( void );
+extern void MITM_run( void );
+
+#endif  /*  SIMTRACE_H  */

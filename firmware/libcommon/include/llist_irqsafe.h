9ac794c770dc22c55759afa45264a863775d67c0
Harald Welte <laforge@gnumonks.org>
2017-05-07 11:19:45 +0200

    Add llist_add_irqsafe() similar to llist_add_tail_irqsafe()

--- firmware/libcommon/include/llist_irqsafe.h
+++ firmware/libcommon/include/llist_irqsafe.h
@@ -2,6 +2,14 @@
 
 #include "osmocom/core/linuxlist.h"
 
+static inline void llist_add_irqsafe(struct llist_head *_new,
+					  struct llist_head *head)
+{
+	__disable_irq();
+	llist_add(_new, head);
+	__enable_irq();
+}
+
 static inline void llist_add_tail_irqsafe(struct llist_head *_new,
 					  struct llist_head *head)
 {

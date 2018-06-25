7be52ec1cefa26c62013293e36f2c9f65f0eac4b
Kévin Redon <kredon@sysmocom.de>
2018-06-25 15:49:28 +0200

    SIMtrace: enable interrupt on edge dection for SIM_RST pin to reset the sniffer ISO state
    Change-Id: I92ff29abd6a8690dd033d8cf13ad0d1ff9740fe5

--- firmware/libboard/simtrace/include/board.h
+++ firmware/libboard/simtrace/include/board.h
@@ -67,7 +67,7 @@
 /* Connect all lines (VPP, CLK, RST, and I/O) between card and phone */
 #define PINS_BUS_SNIFF          PIN_SC_SW_SNIFF, PIN_IO_SW_SNIFF
 /* Card RST reset signal input (use as input since the phone will drive it) */
-#define PIN_SIM_RST_SNIFF      {PIO_PA7, PIOA, ID_PIOA, PIO_INPUT, PIO_DEFAULT}
+#define PIN_SIM_RST_SNIFF      {PIO_PA7, PIOA, ID_PIOA, PIO_INPUT, PIO_DEGLITCH | PIO_IT_EDGE}
 /* Pins used to sniff phone-card communication */
 #define PINS_SIM_SNIFF          PIN_SIM_IO, PIN_SIM_CLK, PIN_SIM_RST_SNIFF
 /* Disable power converter 4.5-6V to 3.3V (active high) */

d5f583da60ab9b589b3ec2c73c28642c27d58130
Kévin Redon <kredon@sysmocom.de>
2018-08-06 15:57:34 +0200

    jenkins: add build uploads
    firmware binaries should be uploaded to
    https://ftp.osmocom.org/binaries/simtrace2/firmware/
    this directory wiki be documented in the SIMtrace 2 wiki for users
    to get the latest firmware images without having to cross-compile
    themselves.
    
    Change-Id: I589531f59e54a0997b012bdd91e3bef9f847f517

--- contrib/jenkins.sh
+++ contrib/jenkins.sh
@@ -54,4 +54,20 @@ make clean
 make
 make clean
 
+if [ "x$publish" = "x--publish" ]; then
+  echo
+  echo "=============== UPLOAD BUILD  =============="
+  mkdir out/
+  cp firmware/bin/*.bin out/
+
+  cat > "$WORKSPACE/known_hosts" <<EOF
+[rita.osmocom.org]:48 ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDDgQ9HntlpWNmh953a2Gc8NysKE4orOatVT1wQkyzhARnfYUerRuwyNr1GqMyBKdSI9amYVBXJIOUFcpV81niA7zQRUs66bpIMkE9/rHxBd81SkorEPOIS84W4vm3SZtuNqa+fADcqe88Hcb0ZdTzjKILuwi19gzrQyME2knHY71EOETe9Yow5RD2hTIpB5ecNxI0LUKDq+Ii8HfBvndPBIr0BWYDugckQ3Bocf+yn/tn2/GZieFEyFpBGF/MnLbAAfUKIdeyFRX7ufaiWWz5yKAfEhtziqdAGZaXNaLG6gkpy3EixOAy6ZXuTAk3b3Y0FUmDjhOHllbPmTOcKMry9
+[rita.osmocom.org]:48 ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBPdWn1kEousXuKsZ+qJEZTt/NSeASxCrUfNDW3LWtH+d8Ust7ZuKp/vuyG+5pe5pwpPOgFu7TjN+0lVjYJVXH54=
+[rita.osmocom.org]:48 ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK8iivY70EiR5NiGChV39gRLjNpC8lvu1ZdHtdMw2zuX
+EOF
+  SSH_COMMAND="ssh -o 'UserKnownHostsFile=$WORKSPACE/known_hosts' -p 48"
+  rsync -avz --delete -e "$SSH_COMMAND" ./out/ binaries@rita.osmocom.org:web-files/simtrace2/firmware/
+  rm -rf out/
+fi
+
 osmo-clean-workspace.sh

c5635780bb0518d5d3977a5b962fc9eaa575151f
Kévin Redon <kredon@sysmocom.de>
2019-05-14 23:39:36 +0200

    make sim switch board specific
    the simtrace board uses a bus switch not used on qmod and owhw to
    switch the SIM between physical and virtual
    
    Change-Id: Ieaf2ed4761fc3e04f33f9aac5c04a768c9a6f71e

--- firmware/libboard/common/include/sim_switch.h
+++ firmware/libboard/common/include/sim_switch.h
@@ -14,5 +14,13 @@
  */
 #pragma once
 
+/** switch card lines to use physical or emulated card
+ *  @param[in] nr card interface number (i.e. slot)
+ *  @param[in] physical which physical interface to switch to (e.g. 0: physical, 1: virtual)
+ *  @return 0 on success, negative else
+ */
 int sim_switch_use_physical(unsigned int nr, int physical);
+/** initialise card switching capabilities
+ *  @return number of switchable card interfaces
+ */
 int sim_switch_init(void);

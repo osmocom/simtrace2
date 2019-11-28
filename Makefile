osmocom/simtrace2

commit 964cda309dc699a293692d5e2549ae80b5b9bd11

host: use autotools and split shared code to libosmo-simtrace2
Change-Id: I57e77f927ee9e169cc794c5dc6b128a2d590201b

host/Makefile.am
@@ -1,0 +1,14 @@
+AUTOMAKE_OPTIONS = foreign dist-bzip2 1.6
+
+AM_CPPFLAGS = $(all_includes) -I$(top_srcdir)/include
+SUBDIRS = include lib src contrib #tests examples doc
+
+EXTRA_DIST = .version git-version-gen
+
+@RELMAKE@
+
+BUILT_SOURCES = $(top_srcdir)/.version
+$(top_srcdir)/.version:
+	echo $(VERSION) > $@-t && mv $@-t $@
+dist-hook:
+	echo $(VERSION) > $(distdir)/.tarball-version

host/contrib/Makefile.am
@@ -1,0 +1,1 @@
+EXTRA_DIST = 99-simtrace2.rules

host/include/Makefile.am
@@ -1,0 +1,7 @@
+nobase_include_HEADERS = \
+		osmocom/simtrace2/apdu_dispatch.h \
+		osmocom/simtrace2/libusb_util.h \
+		osmocom/simtrace2/simtrace2_api.h \
+		osmocom/simtrace2/simtrace_usb.h \
+		osmocom/simtrace2/simtrace_prot.h \
+		osmocom/simtrace2/gsmtap.h

host/lib/Makefile.am
@@ -1,0 +1,19 @@
+# This is _NOT_ the library release version, it's an API version.
+# Please read chapter "Library interface versions" of the libtool documentation
+# before making any modifications: https://www.gnu.org/software/libtool/manual/html_node/Versioning.html
+ST2_LIBVERSION=0:0:0
+
+AM_CPPFLAGS = $(all_includes) -I$(top_srcdir)/include -I$(top_builddir)
+AM_CFLAGS= -Wall $(LIBOSMOCORE_CFLAGS) $(LIBOSMOSIM_CFLAGS) $(LIBUSB_CFLAGS) $(COVERAGE_CFLAGS)
+AM_LDFLAGS = $(COVERAGE_LDFLAGS)
+COMMONLIBS = $(LIBOSMOCORE_LIBS) $(LIBOSMOSIM_LIBS) $(LIBUSB_LIBS)
+
+lib_LTLIBRARIES = libosmo-simtrace2.la
+
+libosmo_simtrace2_la_LDFLAGS = $(AM_LDFLAGS) -version-info $(ST2_LIBVERSION)
+libosmo_simtrace2_la_LIBADD = $(COMMONLIBS)
+libosmo_simtrace2_la_SOURCES = \
+	apdu_dispatch.c \
+	gsmtap.c \
+	libusb_util.c \
+	simtrace2_api.c

host/src/Makefile.am
@@ -1,0 +1,18 @@
+AM_CPPFLAGS = $(all_includes) -I$(top_srcdir)/include
+AM_CFLAGS=-Wall -g $(LIBOSMOCORE_CFLAGS) $(LIBOSMOSIM_CFLAGS) $(LIBUSB_CFLAGS) $(COVERAGE_FLAGS)
+AM_LDFLAGS=$(COVERAGE_LDFLAGS)
+
+LDADD= $(top_builddir)/lib/libosmo-simtrace2.la \
+       $(LIBOSMOCORE_LIBS) $(LIBOSMOSIM_LIBS) $(LIBUSB_LIBS)
+
+noinst_HEADERS = simtrace2-discovery.h
+
+bin_PROGRAMS = simtrace2-remsim simtrace2-remsim-usb2udp simtrace2-list simtrace2-sniff
+
+simtrace2_remsim_SOURCES = simtrace2-remsim.c simtrace2-discovery.c
+
+simtrace2_remsim_usb2udp_SOURCES = usb2udp.c simtrace2-discovery.c
+
+simtrace2_list_SOURCES = simtrace2_usb.c
+
+simtrace2_sniff_SOURCES = simtrace2-sniff.c simtrace2-discovery.c

contrib/jenkins.sh
@@ -50,9 +50,15 @@ make clean
 echo
 echo "=============== HOST START  =============="
 cd $TOPDIR/host
-make clean
-make
-make clean
+autoreconf --install --force
+./configure --enable-sanitize --enable-werror
+$MAKE $PARALLEL_MAKE
+#$MAKE distcheck || cat-testlogs.sh
+make dist
+
+#if [ "$WITH_MANUALS" = "1" ] && [ "$PUBLISH" = "1" ]; then
+#	make -C "$base/doc/manuals" publish
+#fi
 
 if [ "x$publish" = "x--publish" ]; then
 	echo
@@ -67,6 +73,10 @@ EOF
 	rsync --archive --verbose --compress --delete --rsh "$SSH_COMMAND" $TOPDIR/firmware/bin/*-latest.{bin,elf} binaries@rita.osmocom.org:web-files/simtrace2/firmware/latest/
 	rsync --archive --verbose --compress --rsh "$SSH_COMMAND" --exclude $TOPDIR/firmware/bin/*-latest.{bin,elf} $TOPDIR/firmware/bin/*-*-*-*.{bin,elf} binaries@rita.osmocom.org:web-files/simtrace2/firmware/all/
 fi
+
+echo
+echo "=============== HOST CLEAN  =============="
+$MAKE maintainer-clean
 
 echo
 echo "=============== FIRMWARE CLEAN  =============="

host/.gitignore
@@ -1,0 +1,32 @@
+.o
+*.a
+*.lo
+*.la
+.deps
+Makefile
+Makefile.in
+
+#configure
+aclocal.m4
+autom4te.cache/
+compile
+config.guess
+config.log
+config.status
+config.sub
+configure
+configure.lineno
+depcomp
+install-sh
+missing
+stamp-h1
+
+#libtool
+ltmain.sh
+libtool
+.libs
+
+.tarball-version
+.version
+
+*.pc

host/Makefile.old
@@ -5,7 +5,7 @@ APPS=simtrace2-remsim simtrace2-remsim-usb2udp simtrace2-list simtrace2-sniff
 
 all: $(APPS)
 
-simtrace2-remsim: simtrace2-remsim.o apdu_dispatch.o simtrace2-discovery.o libusb_util.o
+simtrace2-remsim: simtrace2-remsim.o apdu_dispatch.o simtrace2-discovery.o simtrace2_api.o libusb_util.o
 	$(CC) -o $@ $^ $(LDFLAGS) `pkg-config --libs libosmosim libpcsclite`
 
 simtrace2-remsim-usb2udp: usb2udp.o simtrace2-discovery.o

commit 71cfc2b90d4088f7bb8a637eeabec49a25fa1d34

Fix topdir Makefile and debian packaging
Make building the debian packages work again. I've verified that it
works in my own OBS namespace.

This patch also adds missing pkgconf variables in host/Makefile.am, so
libosmo-simtrace2.pc installs properly.

Related: OS#4283
Fixes: 964cda309dc699a293692d5e2549ae80b5b9bd11 ("host: use autotools and split shared code to libosmo-simtrace2")
Change-Id: I2377de1e8b149520922217a1ab16f6e22fe6462a

Makefile
@@ -17,10 +17,15 @@ fw-clean: fw-simtrace-dfu-clean fw-simtrace-trace-clean fw-simtrace-cardem-clean
 fw: fw-simtrace-dfu fw-simtrace-trace fw-simtrace-cardem fw-qmod-dfu fw-qmod-cardem
 
 utils:
-	make -C host
+	(cd host && \
+	 autoreconf -fi && \
+	 ./configure --prefix=/usr --disable-werror && \
+	 make)
 
 clean: fw-clean
-	make -C host clean
+	if [ -e host/Makefile ]; then \
+		make -C host clean; \
+	fi
 
 install:
 	make -C firmware install

debian/control
@@ -3,6 +3,13 @@ Maintainer: Harald Welte <laforge@gnumonks.org>
 Section: devel
 Priority: optional
 Build-Depends: debhelper (>= 9),
+               autotools-dev,
+               autoconf,
+               automake,
+               libtool,
+               pkg-config,
+               git,
+               dh-autoreconf,
                libosmocore-dev,
                libpcsclite-dev,
                libnewlib-arm-none-eabi,
@@ -26,6 +33,33 @@ Package: simtrace2-utils
 Section: devel
 Architecture: any
 Multi-Arch: same
-Depends: ${shlibs:Depends}, ${misc:Depends}
+Depends: ${shlibs:Depends}, ${misc:Depends}, libosmo-simtrace2-0
 Recommends: simtrace2-firmware
 Description: Host utilities to communicate with SIMtrace2 USB Devices.
+
+Package: libosmo-simtrace2-0
+Section: libs
+Architecture: any
+Multi-Arch: same
+Depends: ${shlibs:Depends}, ${misc:Depends}
+Description: Osmocom SIMtrace2 library
+  This library contains core "driver" functionality to interface with the
+  Osmocom SIMtrace2 (and compatible) USB device firmware.  It enables
+  applications to implement SIM card / smart card tracing as well as
+  SIM / smart card emulation functions.
+
+Package: libosmo-simtrace2-dev
+Section: libdevel
+Architecture: any
+Multi-Arch: same
+Depends: libosmo-simtrace2-0, ${misc:Depends}
+Description: Development headers for Osmocom SIMtrace2 library
+  This library contains core "driver" functionality to interface with the
+  Osmocom SIMtrace2 (and compatible) USB device firmware.  It enables
+  applications to implement SIM card / smart card tracing as well as
+  SIM / smart card emulation functions.
+  .
+  The header files provided by this package may be used to develop
+  with any of the libosmocore libraries.
+  .
+  Also static libraries are installed with this package.

debian/libosmo-simtrace2-0.install
@@ -1,0 +1,1 @@
+usr/lib/libosmo-simtrace2*.so.*

debian/libosmo-simtrace2-dev.install
@@ -1,0 +1,5 @@
+usr/include/*
+usr/lib/lib*.a
+usr/lib/lib*.so
+usr/lib/lib*.la
+usr/lib/pkgconfig/*

debian/rules
@@ -1,4 +1,19 @@
 #!/usr/bin/make -f
 
+# Uncomment this to turn on verbose mode.
+#export DH_VERBOSE=1
+
+DEBIAN  := $(shell dpkg-parsechangelog | grep ^Version: | cut -d' ' -f2)
+DEBVERS := $(shell echo '$(DEBIAN)' | cut -d- -f1)
+VERSION := $(shell echo '$(DEBVERS)' | sed -e 's/[+-].*//' -e 's/~//g')
+
+export DEB_BUILD_MAINT_OPTIONS = hardening=+all
+
+export DEB_LDFLAGS_MAINT_STRIP = -Wl,-Bsymbolic-functions
+
+
 %:
 	dh $@
+
+override_dh_autoreconf:
+	cd host && dh_autoreconf

host/Makefile.am
@@ -5,6 +5,9 @@ SUBDIRS = include lib src contrib #tests examples doc
 
 EXTRA_DIST = .version git-version-gen
 
+pkgconfigdir = $(libdir)/pkgconfig
+pkgconfig_DATA = libosmo-simtrace2.pc
+
 @RELMAKE@
 
 BUILT_SOURCES = $(top_srcdir)/.version
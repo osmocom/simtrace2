02f01e859c5e6b1345b9d4cb92947077d898ad9a
Harald Welte <laforge@osmocom.org>
2020-01-17 10:25:33 +0100

    host/lib/gsmtap.c: Add GPLv2 disclaimer
    Change-Id: Ie163af869c6aa2f3e2bd9007432dc6945384aaf2

--- host/lib/gsmtap.c
+++ host/lib/gsmtap.c
@@ -1,3 +1,22 @@
+/* gsmtap - How to encapsulate SIM protocol traces in GSMTAP
+ *
+ * (C) 2016-2019 by Harald Welte <hwelte@hmw-consulting.de>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
 #include <osmocom/simtrace2/gsmtap.h>
 
 #include <osmocom/core/gsmtap.h>

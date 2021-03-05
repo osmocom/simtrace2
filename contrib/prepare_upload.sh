1031d9b88441e8ca928f2e5c2205d34c80a1c5ed
Oliver Smith <osmith@sysmocom.de>
2021-03-05 15:38:48 +0100

    firmware: create duplicate files for upload only
    Don't create copies of firmware files with version strings appended in
    the normal build. Only do this before uploading the firmware files.
    
    I have verified that "make" before this change and
    "make; contrib/prepare_upload.sh" after produce the same files.
    
    Close: OS#4413
    Change-Id: I118a4ff397a178281c26a6b98112fa66b6f049ab

---
+++ contrib/prepare_upload.sh
@@ -0,0 +1,14 @@
+#!/bin/sh -e
+# Create copies of binaries with -latest, -$GIT_VERSION (OS#4413, OS#3452)
+GIT_VERSION="$(./git-version-gen .tarball-version)"
+
+echo "Copying binaries with "-latest" and "-$GIT_VERSION" appended..."
+
+cd firmware/bin
+for ext in bin elf; do
+	for file in *."$ext"; do
+		without_ext="${file%.*}"
+		cp -v "$file" "$without_ext-latest.$ext"
+		cp -v "$file" "$without_ext-$GIT_VERSION.$ext"
+	done
+done

f721e69bc1d2adc7228b479a5ebec617469933ff
Oliver Smith <osmith@sysmocom.de>
2021-12-14 11:45:00 +0100

    treewide: remove FSF address
    Remove the paragraph about writing to the Free Software Foundation's
    mailing address. The FSF has changed addresses in the past, and may do
    so again. In 2021 this is not useful, let's rather have a bit less
    boilerplate at the start of source files.
    
    Change-Id: Ie0a3b2273383adbb3303faffd6ff96be7f4cae99

--- firmware/libboard/qmod/source/wwan_perst.c
+++ firmware/libboard/qmod/source/wwan_perst.c
@@ -9,10 +9,6 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307, USA
  */
 /* Depending on the board this is running on,  it might be possible
  * for the controller to set the status of the PERST input line of
